068f11d27c69485cb6bea0a8ef476d2224dbec07
ytkaymak <126457720+ytkaymak@users.noreply.github.com>
2023-03-20 23:29:44 +0300

    hesap makinesi

---
+++ hesap makinesi/script.js
@@ -0,0 +1,28 @@
+function add() {
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    var result = num1 + num2;
+    alert(result);
+  }
+  
+  function subtract() {
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    var result = num1 - num2;
+    alert(result);
+  }
+  
+  function multiply() {
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    var result = num1 * num2;
+    alert(result);
+  }
+  
+  function divide() {
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    var result = num1 / num2;
+    alert(result);
+  }
+  
